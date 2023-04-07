bc44ea5f8153ec6ffc47149c389a2b4a922a8fd9
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-04-07 10:38:58 +0300

    3 пример сложения чисел

---
+++ Exampele_003/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int sum = numberA + numberB;
+Console.Write(sum);
+*/
+int numberA = 3;
+int numberB =5;
+int result = numberA + numberB;
+Console.WriteLine(result);
