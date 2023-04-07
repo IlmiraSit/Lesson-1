92713353cae310650575a98b71cb98ff2e2f3f09
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-04-07 11:03:55 +0300

    5 пример

---
+++ Exampele_005/Program.cs
@@ -0,0 +1,19 @@
+﻿ /*
+ int numberA = new Random().Next(1, 10);
+ Console.WriteLine(numberA);
+ int  numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+*/
+Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
